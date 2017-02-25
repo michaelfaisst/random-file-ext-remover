d0e767e07d7e27f08570f60b4b5e26a187fa3746
Michael Faisst <michael@faisst.at>
2017-02-25 15:02:35 +0100

    First commit

---
+++ RandomFileExtRemover/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace RandomFileExtRemover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnSelectPath = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.Location = new System.Drawing.Point(12, 38);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(718, 537);
+            this.lbxOutput.TabIndex = 0;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 11);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(438, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnSelectPath
+            // 
+            this.btnSelectPath.Location = new System.Drawing.Point(456, 9);
+            this.btnSelectPath.Name = "btnSelectPath";
+            this.btnSelectPath.Size = new System.Drawing.Size(134, 23);
+            this.btnSelectPath.TabIndex = 2;
+            this.btnSelectPath.Text = "Pfad auswählen";
+            this.btnSelectPath.UseVisualStyleBackColor = true;
+            this.btnSelectPath.Click += new System.EventHandler(this.btnSelectPath_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(596, 9);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(134, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Ausführen";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 580);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnSelectPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lbxOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileExt Entferner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxOutput;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnSelectPath;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ RandomFileExtRemover/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomFileExtRemover
+{
+    public partial class Form1 : Form
+    {
+
+        private string _selectedPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnGo.Enabled = false;
+        }
+
+        private void btnSelectPath_Click(object sender, EventArgs e)
+        {
+            var result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+            {
+                _selectedPath = folderBrowserDialog.SelectedPath;
+                txtPath.Text = _selectedPath;
+                btnGo.Enabled = true;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            var result =
+                MessageBox.Show(
+                    "Es wird empfohlen vor Ausführung der Umbenennung eine Sicherungskopie des Ordners zu machen. Fortfahren?",
+                    "", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+
+                var allFilesPaths = Directory.GetFiles(_selectedPath, "*.*", SearchOption.AllDirectories);
+
+                lbxOutput.Items.Clear();
+                foreach (var filePath in allFilesPaths)
+                {
+                    var lastDotIndex = filePath.LastIndexOf(".", StringComparison.InvariantCulture);
+
+                    if (filePath.Length - lastDotIndex - 1 == 6)
+                    {
+                        var newFileName = filePath.Substring(0, lastDotIndex);
+                        lbxOutput.Items.Add("Datei gefunden: " + filePath);
+                        lbxOutput.Items.Add("Neuer Dateiname: " + newFileName);
+
+                        File.Move(filePath, newFileName);
+                        lbxOutput.Items.Add("Erfolgreich umbenannt");
+
+                        lbxOutput.Items.Add("");
+                    }
+                }
+
+                MessageBox.Show("Alle Dateien erfolgreich umbenannt");
+            }
+        }
+    }
+}
